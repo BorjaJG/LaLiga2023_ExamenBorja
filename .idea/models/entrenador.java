3b84ae9d4151650dd514eed530061b8c1648c125
10.055 <borjajg02@gmail.com>
2023-09-04 19:50:36 +0200

    Añadido los atributos de las clases

---
+++ .idea/models/entrenador.java
@@ -0,0 +1,13 @@
+
+public class entrenador{
+
+    private Date anoNacimiento;
+
+    public Date getAnoNacimiento() {
+        return anoNacimiento;
+    }
+
+    public void setAnoNacimiento(Date anoNacimiento) {
+        this.anoNacimiento = anoNacimiento;
+    }
+}
---
+++ .idea/models/equipo.java
@@ -0,0 +1,40 @@
+
+public class equipo{
+
+    private String id;
+    private String nombre;
+   private String  telefono;
+    private string direccion;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public string getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(string direccion) {
+        this.direccion = direccion;
+    }
+}
