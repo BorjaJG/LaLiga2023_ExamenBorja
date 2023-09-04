b07e59af97437c04a325744115654b3a8e59a5a1
10.055 <borjajg02@gmail.com>
2023-09-04 20:25:31 +0200

    Añadido las demos

--- .idea/presentacion/main.java
+++ .idea/presentacion/main.java
@@ -1,10 +1,65 @@
 
 public class main{
 
-    public Static void main (String[] args){
+    public static  void main (String[] args){
 
-        persona.p1 = new pesona;
-        p1.getnombre
+        persona.p1 = new persona;
+        p1.getNombre("pedro");
+        p1.getApellido1("jiminez");
+        p1.getApellido1("perez");
+
+        entrenador.p1 = new entrenador;
+        p1.getAnoNacimiento("2023-02-11");
+
+        persona.p2 = new persona;
+        p2.getNombre("pedro2");
+        p2.getApellido1("jiminez2");
+        p2.getApellido1("perez2");
+
+        jugador.p2 = new jugador;
+        p2.getNumCamiseta(23);
+
+        persona.p4 = new persona;
+        p4.getNombre("pedro4");
+        p4.getApellido1("jiminez4");
+        p4.getApellido1("perez4");
+
+
+        presidente.p4 = new presidente;
+        p4.getProfesion("Presidente");
+
+        equipo.eq1= new equipo;
+        eq1.getID("2433t");
+        eq1.getNombre("Sevilla");
+            eq1.getTelefono("293 429 42");
+        eq1.getDirecion("pepe n2");
+
+
+        partido.part1 = new partido;
+        part1.getId("92394g2");
+        part1.getFechaPartido("2023-11-02");
+        part1.getEquipoLocal("sevilla");
+        part1.getEquipoVisitante("granada");
+
+
+        System.out.println("Equipo :" + );
+        System.out.println("" + );
+        System.out.println("" + );
+        System.out.println("" + );
+        System.out.println("" + );
+        System.out.println("" + );
+        System.out.println("" + );
+        System.out.println("" + );
+        System.out.println("" + );
+        System.out.println("" + );
+        System.out.println("" + );
+        System.out.println("" + );
+        System.out.println("" + );
+        System.out.println("" + );
+        System.out.println("" + );
+
+
+
 
 
 
